c6760ee9ca0193bcab1f10eabd4dcfc01694cfcd
Antimateria117 <sergi2312@gmail.com>
2017-05-28 15:59:21 +0200

    Add files via upload

--- ACR350.cpp
+++ ACR350.cpp
@@ -8,13 +8,9 @@ double const pi = 3.141592654;
 
 int main () {
 	int a,b;
-	double area1, area2, areaMax;
-	cin >> a >> b;
-	while (a != 0 && b != 0) {
-		area1 = 0.5*a*b*cos((pi/2)/180);
-		area2 = 0.5*a*b*cos((3*pi/2)/180);
-		areaMax = fmax(area1, area2);
-		printf("%f\n", areaMax);
+	double area;
+	while (cin >> a >> b && a || b) {
+		printf("%.1f\n", a*b/2.0);
 		cin >> a >> b;
 	}
 	return 0;
