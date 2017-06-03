0da4bbedde7b24f3ea170d5484818a910f955822
Sergio <sergi2312@gmail.com>
2017-06-03 23:55:44 +0200

    Not solved yet

---
+++ ACR132.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main (void) {
+  while (true) {
+    string frase;
+    cin >> frase;
+
+    int numCasos;
+    cin >> numCasos;
+
+    if (numCasos == 0) break;
+
+    int i,j = 0;
+    string subCadena;
+    bool iguales;
+
+    for (int k=0;k<numCasos;k++){
+      iguales = true;
+      cin >> i >> j;
+      subCadena = frase.substr(fmin(i,j), (fmax(i,j)-fmin(i,j))+1);
+      char primeraLetra = subCadena[0];
+      for(int a=fmax(i,j)-fmin(i,j);a>=0;a--){
+        if (primeraLetra != subCadena[a]) iguales = false;
+      }
+      if (iguales)
+        cout << "SI" << endl;
+      else
+        cout << "NO" << endl;
+    }
+    cout << endl;
+    cin.ignore(256, '\n');
+  }
+  return 0;
+}
