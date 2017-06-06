a2be0525fe1b7c3766f1a47db3fe9fec5f378eb2
Sergio <sergi2312@gmail.com>
2017-06-06 12:41:12 +0200

    Not solved yet (WA) - don't know why

---
+++ ACR222.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ACR222 {
+  public static void main (String [] args) {
+    Scanner in = new Scanner(System.in);
+    int b,p,s;
+    while (in.hasNextInt()){
+      b = in.nextInt();
+      p = in.nextInt();
+      if (b==1)
+        System.out.println(p+1);
+      else{
+		s = ((1-(int)Math.pow(b,p+1))/(1-b))%1000007;
+        System.out.println(s);
+	  }
+    }
+  }
+}
